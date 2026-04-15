b4ee9d34303b1cdf17f76d5a086f932e5bd9bc82
CindyPuspaA01 <alfanicff@gmail.com>
2026-04-16 00:26:04 +0700

    Add files via upload

---
+++ UTSPart1SelectionSort.java
@@ -0,0 +1,32 @@
+public class UTSPart1SelectionSort {
+    public static void main(String[] args) {
+
+        String[] nama = {"Aziz","Artha","Fino","Fajar","Farida","Tiara","Nawang","Cia","Hidayat","Slamet" };
+        int[] nilai = {88,75,92,80,85,90,87,78,95,91};
+
+        for (int i = 0; i < nilai.length - 1; i++) {
+
+            int maxIndex = i;
+
+            for (int j = i + 1; j < nilai.length; j++) {
+                if (nilai[j] > nilai[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+
+            int tempNilai = nilai[i];
+            nilai[i] = nilai[maxIndex];
+            nilai[maxIndex] = tempNilai;
+
+            String tempNama = nama[i];
+            nama[i] = nama[maxIndex];
+            nama[maxIndex] = tempNama;
+
+            System.out.println("\nIterasi " + (i + 1));
+            System.out.println("No  Nama                 Nilai");
+            for (int k = 0; k < nama.length; k++) {
+                System.out.printf("%-3d %-20s %d\n", k+1, nama[k], nilai[k]);
+            }
+        }
+    }
+}
